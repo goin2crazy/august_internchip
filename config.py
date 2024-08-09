736912393d19c5808b39270bb262b1078d159745
Double Cringe <92397500+goin2crazy@users.noreply.github.com>
2024-08-09 23:59:56 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ model_exct_path= '.'
 
 model_feat_path='doublecringe123/lora_job_features_extractor_flant5_v2' 
 
-model_feat_versions = 'f2df1d0b06bce3ac1a7ecf7d9408737efb416118'
+model_feat_versions = None # newest
 # f2df1d0b06bce3ac1a7ecf7d9408737efb416118 - mini model, no prompt
 # 00e8801ca3d4314a1d2cb0be101440ce738dd129 - mini model with prompt
 # f67bc736ef24991e646c96b6993c4d44f2d12078 - base model with prompt
